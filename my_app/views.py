166e720ca103d99e5c7e9c4367c2114abac1bbdb
Ray <873762491@qq.com>
2020-06-25 00:51:22 +0100

    html left

--- my_app/views.py
+++ my_app/views.py
@@ -6,6 +6,7 @@ from . import models
 
 # BASE_URL = 'http://university.bath.org/search/?query={}'
 BASE_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
 def home(request):
@@ -37,7 +38,16 @@ def new_search(request):
         else:
             post_price = 'N/A'
 
-        final_postings.append((post_title, post_url, post_price))
+        # found images
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            # no image there
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
 
     stuff_for_frontend = {
         'search': search,
