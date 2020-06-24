349147f6d812b61d42806ffdc42064f2827c7656
Ray <873762491@qq.com>
2020-06-24 13:48:59 +0100

    back

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+# from bs4 import BeautifulSoup
+# import requests
+
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    # get the POST request from the "new_search.html, name='search'", POST is a dictionary.
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+    }
+
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
